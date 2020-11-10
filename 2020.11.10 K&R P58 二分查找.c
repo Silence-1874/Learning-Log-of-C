d4cca6339fcab74195633e392e80614542de2407
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2020-11-10 22:19:25 +0800

    Create 2020.11.10 K&R P58 二分查找.c

---
+++ 2020.11.10 K&R P58 二分查找.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int binsearch(int x, int v[], int n)
+{
+	int low, high, mid;
+	low = 0;
+	high = n = 1;
+	while (low <= high) {
+		mid = (low + high) / 2;
+		if (x < v[mid])
+			high = mid - 1;
+		else if (x > v[mid])
+			low = mid + 1;
+		else
+			return mid;
+	}
+	return -1;
+}
