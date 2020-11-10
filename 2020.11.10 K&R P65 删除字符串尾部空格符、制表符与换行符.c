cbc687a47150073e026e5a653e60d30b8633946c
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2020-11-10 23:10:32 +0800

    Create 2020.11.10 K&R P65 删除字符串尾部空格符、制表符与换行符.c

---
+++ 2020.11.10 K&R P65 删除字符串尾部空格符、制表符与换行符.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int trim(char s[])
+{
+	int n;
+	for (n = strlen(s) - 1; n >= 0; n--)
+		if (s[n] != ' ' && s[n] != '\t' && s[n] != '\n')
+			break;
+	s[n + 1] = '\0';
+	return n;
+}
