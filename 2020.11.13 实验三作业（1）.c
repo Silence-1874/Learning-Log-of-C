67f40ce4e44e16fb51b29e9de98ed97eeecccb28
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2020-11-13 13:13:27 +0800

    Create 2020.11.13 实验三作业（1）.c

---
+++ 2020.11.13 实验三作业（1）.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>						//为获取更精确的结果
+int main()
+{
+	double d,p,r,m;					//定义局部变量d，p，r，m
+	d = 300000;					//初始化d
+	p = 6000;						//初始化p
+	r = 0.01;						//初始化r
+	m = log10(p /( p - d * r)) /log10 (1 + r);			//计算m
+	printf("%.1lf月能还清",m);				//输出结果
+	return 0;
+}
