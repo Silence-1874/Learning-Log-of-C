305260d4e7f41840175f3db1b38d8ee313378886
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2020-12-05 15:25:05 +0800

    Create 2020.12.05 顺序栈的基本操作.c

---
+++ 2020.12.05 顺序栈的基本操作.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#define MAXSIZE 100
+int main()
+{
+	typedef struct Node {
+		int data[MAXSIZE];	//数据域
+		int top;			//栈顶标记
+	}Stack;
+	Stack stack;			//定义栈
+	stack.top = -1;			//初始top为-1，记为栈空
+
+	int bool = 1;
+	while (bool) {
+		printf("请输入要压入的元素: ");
+		int x;
+		scanf_s("%d", &x);
+		if (stack.top >= MAXSIZE - 1) {	//判断是否栈满
+			printf("栈满！\n");
+			break;
+		} else {
+			stack.data[++stack.top] = x;//压入一个元素，并将栈顶标记向后移一位
+		}
+		printf("输入1继续圧栈，输入0结束圧栈： ");
+		scanf_s("%d", &bool);
+	}
+
+	printf("当前栈内元素为\n");
+	for (int cnt = stack.top; cnt >= 0; cnt--) {
+		printf("%d\n", stack.data[cnt]);
+	}
+
+	printf("输入1开始出栈，输入0结束操作: ");
+	scanf_s("%d", &bool);
+	while (bool) {
+		if (stack.top == -1) {			//判断是否栈空
+			printf("栈空！\n");
+			break;
+		} else {
+			printf("“%d”出栈！\n", stack.data[stack.top]);	//弹出栈顶元素
+			stack.top--;									//并将栈顶标记向前移一位
+		}
+		printf("输入1继续出栈，输入0结束出栈: ");
+		scanf_s("%d", &bool);
+	}
+
+	printf("当前栈内元素为\n");
+	for (int cnt = stack.top; cnt >= 0; cnt--) {
+		printf("%d\n", stack.data[cnt]);
+	}
+
+	return 0;
+}
