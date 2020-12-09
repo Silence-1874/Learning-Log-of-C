418965ab02cde05770551a3b2fb1c6ce6a9f736c
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2020-12-09 15:34:07 +0800

    Create 2020.12.09 二叉树的中序遍历（递归）.c

---
+++ 2020.12.09 二叉树的中序遍历（递归）.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct BiTNode {
+	char data;
+	struct BiTNode* Lchild;
+	struct BiTNode* Rchild;
+}BiTNode, * BiTree;
+void CreateBiTree(BiTree*);
+void PreOrderTraverse(BiTree);
+int main()
+{
+	BiTree Tree;
+	CreateBiTree(&Tree);
+	PreOrderTraverse(Tree);
+	return 0;
+}
+void PreOrderTraverse(BiTree T) {	//中序遍历：左→根→右
+	if (T) {
+		PreOrderTraverse(T->Lchild);
+		printf("%c", T->data);
+		PreOrderTraverse(T->Rchild);
+	}
+	else putchar('#');
+	return;
+}
+void CreateBiTree(BiTree* T)
+{
+	*T = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->data = 'A';
+	(*T)->Lchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->Lchild->data = 'B';
+	(*T)->Rchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->Rchild->data = 'C';
+	(*T)->Lchild->Lchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->Lchild->Lchild->data = 'D';
+	(*T)->Lchild->Rchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->Lchild->Rchild->data = 'E';
+	(*T)->Rchild->Lchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->Rchild->Lchild->data = 'F';
+	(*T)->Rchild->Rchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->Rchild->Rchild->data = 'G';
+	(*T)->Lchild->Lchild->Lchild = NULL;
+	(*T)->Lchild->Lchild->Rchild = NULL;
+	(*T)->Lchild->Rchild->Lchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->Lchild->Rchild->Lchild->data = 'H';
+	(*T)->Lchild->Rchild->Lchild->Lchild = NULL;
+	(*T)->Lchild->Rchild->Lchild->Rchild = NULL;
+	(*T)->Lchild->Rchild->Rchild = NULL;
+	(*T)->Rchild->Lchild->Lchild = NULL;
+	(*T)->Rchild->Lchild->Rchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->Rchild->Lchild->Rchild->data = 'I';
+	(*T)->Rchild->Lchild->Rchild->Lchild = NULL;
+	(*T)->Rchild->Lchild->Rchild->Rchild = NULL;
+	(*T)->Rchild->Rchild->Lchild = NULL;
+	(*T)->Rchild->Rchild->Rchild = NULL;
+}
