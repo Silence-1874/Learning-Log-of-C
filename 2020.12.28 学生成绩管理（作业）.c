c8b862efee2bced5323b62680b28327b8af7e5d7
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-01-02 20:35:58 +0800

    Update 2020.12.28 学生成绩管理（作业）.c

--- 2020.12.28 学生成绩管理（作业）.c
+++ 2020.12.28 学生成绩管理（作业）.c
@@ -7,14 +7,14 @@ typedef struct s {	//定义结构体s,储存学生数据
 	int score[3];	//储存3门课的成绩
 	int n;			//储存学生学号
 	char name[10];	//储存学生姓名
-}*stu;				//将指向该结构体的指针类型命名为stu
+}*Stu;				//将指向该结构体的指针类型命名为Stu
 
-void avg(stu, int);	//声明求平均分函数avg
-int search(stu);	//声明排序函数sort
-void output(stu);	//声明输出函数output
+void avg(Stu, int);	//声明求平均分函数avg
+int search(Stu);	//声明排序函数sort
+void output(Stu);	//声明输出函数output
 int main()
 {
-	stu p = (stu)malloc(sizeof(struct s) * NUM);	//初始化指针p
+	Stu p = (Stu)malloc(sizeof(struct s) * NUM);	//初始化指针p
 
 	for (int i = 0; i < NUM; i++) {					//初始化学生数据
 		printf("请输入第%d名学生的姓名:\n", i + 1);
@@ -32,7 +32,7 @@ int main()
 	return 0;
 }
 
-void avg(stu p, int x)
+void avg(Stu p, int x)
 {
 	double sum = 0;
 	for (int i = 0; i < NUM; i++) {
@@ -41,7 +41,7 @@ void avg(stu p, int x)
 	printf("第%d门课的平均分为 %lf\n", x + 1, sum / NUM);
 }
 
-int search(stu p)					//顺序查找
+int search(Stu p)					//顺序查找
 {
 	int max = 0;
 	for (int i = 0; i < NUM; i++) {
@@ -53,7 +53,7 @@ int search(stu p)					//顺序查找
 	return max;
 }
 
-void output(stu p)
+void output(Stu p)
 {
 	printf("分数最高的学生为%s\n", p->name);
 	printf("学号为%d\n", p->n);
