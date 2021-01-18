d04b9a1a0f4ae92263aadff0e6e52a4976253eb7
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-01-18 20:28:38 +0800

    Create 2021.01.18 试卷2.1.c

---
+++ 2021.01.18 试卷2.1.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+struct
+{
+	char name[20];
+	int number;
+	char sex;
+	union
+	{
+		char eye;
+		struct
+		{
+			int height;
+			int weight;
+		}f;
+	}body;
+}per[2];
+
+int main()
+{
+	int i;
+	for (i = 0; i < 2; i++)
+	{
+		scanf("%s %d %c", &per[i].name, &per[i].number, &per[i].sex);
+		if (per[i].sex == 'M')
+		{
+			scanf("%c", &per[i].body.eye);
+		}
+		else if (per[i].sex == 'f')
+		{
+			scanf("%d %d", &per[i].body.f.height, &per[i].body.f.weight);
+		}
+		else
+		{
+			printf("input error!\n");
+		}
+	}
+}
